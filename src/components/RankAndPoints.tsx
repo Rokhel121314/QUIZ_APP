b479083f50d604e9c3f4e4f59ba7beada1b88758
jerickwebdev <jerickddelacruzwebdev@gmail.com>
2023-08-11 20:51:01 +0800

    created title and rankingpoints component

---
+++ src/components/RankAndPoints.tsx
@@ -0,0 +1,33 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+const RankAndPoints = () => {
+  return (
+    <View className="h-16 bg-gray-50 mt-8 rounded-2xl flex-row items-center justify-center shadow-lg shadow-black">
+      <View className="flex-row items-center justify-center w-1/2">
+        <View className="w-1/2 flex-row justify-end pr-5">
+          <Icon name="trophy" size={38} color={'#F6C000'} />
+        </View>
+        <View className="w-1/2">
+          <Text className="font-xs font-semibold">Ranking</Text>
+          <Text className="text-blue-500 text-2xl font-semibold">500</Text>
+        </View>
+      </View>
+
+      <View className="border-l h-12 border-gray-300"></View>
+
+      <View className="flex-row items-center justify-center w-1/2">
+        <View className="w-1/2 flex-row justify-end pr-5">
+          <Icon name="dollar-sign" size={38} color={'#F6C000'} />
+        </View>
+        <View className="w-1/2">
+          <Text className="font-xs font-semibold">Points</Text>
+          <Text className="text-blue-500 text-2xl font-semibold">1200</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default RankAndPoints;
--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,15 +1,18 @@
-import {View, Text} from 'react-native';
+import {View, Text, SafeAreaView} from 'react-native';
 import React from 'react';
 import {RootStackScreenProps} from '../navigation/navigationType';
+import Title from '../components/Title';
+import RankAndPoints from '../components/RankAndPoints';
 
 const ProfileScreen = ({
   route,
   navigation,
 }: RootStackScreenProps<'Profile'>) => {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 px-5">
+      <Title mainText="Hi, Jerick!" subText="Lets make this day productive!" />
+      <RankAndPoints />
+    </SafeAreaView>
   );
 };
 
