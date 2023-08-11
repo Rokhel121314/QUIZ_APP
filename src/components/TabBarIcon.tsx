a73a4b8f561aa3b519708dab13a196d83fe7612f
jerickwebdev <jerickddelacruzwebdev@gmail.com>
2023-08-11 15:36:03 +0800

    deleted output.css

--- src/components/TabBarIcon.tsx
+++ src/components/TabBarIcon.tsx
@@ -1,17 +1,21 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import Feather from 'react-native-vector-icons/Feather';
 
-const TabBarIcon = ({
-  focused,
-  routeName,
-}: {
+type Props = {
   focused: boolean;
   routeName: string;
-}) => {
+  iconName: string;
+};
+
+const TabBarIcon = ({focused, routeName, iconName}: Props) => {
   return (
-    <View className="items-center justify-center">
+    <View className="items-center justify-center flex-col">
+      <Feather name={iconName} size={25} color={focused ? '#000' : '#d1d1d1'} />
       <Text
-        className={`text-sm ${focused ? 'text-gray-950' : 'text-gray-400'}`}>
+        className={`text-sm ${
+          focused ? 'text-gray-950 font-bold' : 'text-gray-400'
+        }`}>
         {routeName}
       </Text>
     </View>
